aab4ecf2d9c7889d679947db6ed2bf2fef64508b
blackfoxDev <blackfoxdev1@gmail.com>
2017-09-05 11:42:19 -0700

    Create Minimum_Spanning_Tree.py

---
+++ Algorithms/Minimum_Spanning_Tree.py
@@ -0,0 +1,32 @@
+# This method finds the MST of a graph using
+# Prim's Algorithm
+# returns the weight of the MST
+def find_min_spanning_tree(self):
+  vertex_count = 0
+  weight = 0
+
+  # Add first vertex to the MST
+  current = self.g[0]
+  current.visited = True
+  vertex_count += 1
+
+  # Construct the remaining MST using the
+  # smallest weight edge
+  while vertex_count < len(self.g):
+    smallest = None
+    for i in range(len(self.e)):
+      if self.e[i].visited == False and self.e[i].dest.visited == False:
+        smallest = self.e[i]
+        break
+        
+    for i in range(len(self.e)):
+      if self.e[i].visited == False:
+        if self.e[i].src.visited == True and self.e[i].dest.visited == False and self.e[i].weight < smallest.weight:
+          smallest = self.e[i]
+
+    smallest.visited = True
+    smallest.dest.visited = True
+    weight += smallest.weight
+    vertex_count += 1
+      
+  return weight
